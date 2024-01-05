b7d0cdd9eed212d6cb0a57e3d8d4986a74eaff5c
Katharina-Beck <katja131291@gmail.com>
2024-01-05 14:35:25 +0100

    added: functionality

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const p = fetch("https://dummy-apis.netlify.app/api/quote");
+const btnQuote = document.querySelector("#btnQuote");
+const quote = document.querySelector("#quote");
+const name = document.querySelector("#name");
 
-p.then((response) => {
-  console.log(response.status);
-  console.log(response.ok);
-  return response.json();
-})
-.then((data) => {
-  console.log(data);
+btnQuote.addEventListener("click", function () {
+  const p = fetch("https://dummy-apis.netlify.app/api/quote");
+
+  p.then((response) => {
+    // console.log(response.status);
+    // console.log(response.ok);
+    return response.json();
+  }).then((data) => {
+    // console.log(data);
+
+    quote.innerHTML = data.quote;
+    name.innerHTML = "-" + data.author;
+  });
 });
