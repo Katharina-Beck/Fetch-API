6eb32b038458313089aad7bb83d762d82a9ab2fe
Katharina-Beck <katja131291@gmail.com>
2024-01-05 16:43:33 +0100

    added> function to button and solved task

--- color-mixer-with-API/script.js
+++ color-mixer-with-API/script.js
@@ -6,8 +6,21 @@ updateColor();
 ranges.forEach((range) => range.addEventListener("input", updateColor));
 
 function updateColor() {
-  console.log("hallo");
   const color = `rgb(${ranges[0].value}, ${ranges[1].value}, ${ranges[2].value}) `;
   output.style.backgroundColor = color;
   colorCode.innerHTML = color;
 }
+
+btnRandom.addEventListener("click", function () {
+  const p = fetch("https://dummy-apis.netlify.app/api/color");
+
+  p.then((response) => {
+    return response.json();
+  }).then((data) => {
+    ranges[0].value = data.rgb.r;
+    ranges[1].value = data.rgb.g;
+    ranges[2].value = data.rgb.b;
+
+    updateColor();
+  });
+});
