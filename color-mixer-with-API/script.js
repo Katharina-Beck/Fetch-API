744ea63094184c7d644b70fe1a61e6b7b87b4de9
Katharina-Beck <katja131291@gmail.com>
2024-01-05 14:54:25 +0100

    added: old task an fixed it

---
+++ color-mixer-with-API/script.js
@@ -0,0 +1,13 @@
+const output = document.querySelector(".output");
+const ranges = document.querySelectorAll("input[type=range]");
+const colorCode = document.querySelector(".colorCode");
+
+updateColor();
+ranges.forEach((range) => range.addEventListener("input", updateColor));
+
+function updateColor() {
+  console.log("hallo");
+  const color = `rgb(${ranges[0].value}, ${ranges[1].value}, ${ranges[2].value}) `;
+  output.style.backgroundColor = color;
+  colorCode.innerHTML = color;
+}
